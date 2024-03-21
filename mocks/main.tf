dff06ea17e4f5c1230b14ef7d4ef96b57b802509
Daniel Schmidt <danielmschmidt92@gmail.com>
2024-03-21 12:05:10 +0100

    only static data

--- mocks/main.tf
+++ mocks/main.tf
@@ -9,11 +9,7 @@ terraform {
   }
 }
 
-data "tfcoremock_simple_resource" "my_simple_resource" {
-  id = "my-simple-resource"
-}
-
 resource "tfcoremock_dynamic_resource" "my_dynamic_resource" {
   id = "my-dynamic-resource"
-  my_value = data.tfcoremock_simple_resource.my_simple_resource.integer + 1
+  my_value = 42
 }
