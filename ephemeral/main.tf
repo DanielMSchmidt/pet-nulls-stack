35c96285a5f1c71be1083160ce23701b1f79cf5f
Daniel Schmidt <danielmschmidt92@gmail.com>
2024-11-11 17:00:22 +0100

    add a resource

--- ephemeral/main.tf
+++ ephemeral/main.tf
@@ -1,6 +1,25 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.3.2"
+    }
+  }
+}
+
 variable "ephem" {
     type      = string
     ephemeral = true
+}
+
+variable "prefix" {
+  type = string
+  default = "ephemeral_"
+}
+
+resource "random_pet" "this" {
+  prefix = var.prefix
+  length = 10
 }
 
 output "out" {
